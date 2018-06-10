52725694df83e906d113347808c2f3e3d0d2b315
Jozue06 <jozue06@gmail.com>
2018-06-10 01:54:14 -0700

    readme

--- arrays/array_adjacent_product.js
+++ arrays/array_adjacent_product.js
@@ -1,10 +1,10 @@
-'use strict';
+// 'use strict';
 
-module.exports = exports = {};
+// module.exports = exports = {};
 
-exports.array_adjacent_product = (arr) => {
+// exports.array_adjacent_product = (arr) => {
     
 
-};
+// };
   
 
